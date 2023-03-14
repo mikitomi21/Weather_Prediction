d13f69632f51f9756b8d5f4afd12314a8fabf661
mikitomi21 <kubastach2001@gmail.com>
2023-03-14 18:08:02 +0100

    Add posibility to change frac of splitting data

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ def get_data():
     #y_data = df["t9"].to_numpy()
     return df
 
-def split_data(dataset):
-    train_dataset = dataset.sample(frac=0.8, random_state=0)
+def split_data(dataset, frac_set=0.8):
+    train_dataset = dataset.sample(frac=frac_set, random_state=0)
     test_dataset = dataset.drop(train_dataset.index)
 
     return train_dataset, test_dataset
