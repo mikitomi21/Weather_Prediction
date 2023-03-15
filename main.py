c046c7412089b3636cd710f3e4b1841b9fd72c2a
mikitomi21 <kubastach2001@gmail.com>
2023-03-15 10:01:46 +0100

    Create a model to predict temperature by avg temp from previous day

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ data = get_data()
 
 train_data, test_data = split_data(data)
 
-x_train = train_data["Avg_Temp"].to_numpy()
-y_train = train_data["Avg_Temp_Pre_Day"].to_numpy()
+x_train = train_data["Avg_Temp_Pre_Day"].to_numpy()
+y_train = train_data["Avg_Temp"].to_numpy()
 
-x_test = test_data["Avg_Temp"].to_numpy()
-y_test = test_data["Avg_Temp_Pre_Day"].to_numpy()
+x_test = test_data["Avg_Temp_Pre_Day"].to_numpy()
+y_test = test_data["Avg_Temp"].to_numpy()
 
 linear = Linear_reg()
 linear.fit(x_train, y_train)
@@ -23,6 +23,6 @@ x = np.linspace(min(x_test), max(x_test), 100)
 y = float(linear.get_theta()[0]) + float(linear.get_theta()[1]) * x
 plt.plot(x, y)
 plt.scatter(x_test, y_test)
-plt.xlabel('Avg_Temp')
-plt.ylabel('Avg_Temp_Pre_Day')
+plt.xlabel('Avg_Temp_Pre_Day')
+plt.ylabel('Avg_Temp')
 plt.show()
