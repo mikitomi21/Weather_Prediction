9092f35817e801b7332855034d431c6499476e0b
mikitomi21 <kubastach2001@gmail.com>
2023-03-22 14:38:45 +0100

    Add feeding forward to neural network

--- network_model.py
+++ network_model.py
@@ -7,3 +7,22 @@ class NeuralNetwork:
         self.weight2 = np.random.rand(n_neurons, 1)
         self.y = y
         self.output = np.zeros(np.shape(y)[0])
+    
+    def feed_forward(self):
+        self.label = np.maximum(0, np.dot(self.input, self.weight1))
+        self.output = np.maximum(0, np.dot(self.label, self.weight2))
+
+X = np.array([1,2,3,4,5,6])
+X = np.reshape(X, (np.shape(X)[0], 1))
+y = np.array([2,3,4,5,6,5])
+network = NeuralNetwork(X, y, 5)
+
+print(network.input)
+print(network.weight1)
+print(network.weight2)
+print(network.y)
+print(network.output)
+
+network.feed_forward()
+
+print(network.output)
