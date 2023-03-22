d44186caf07eb83e12022682c78848bd8e8c9603
mikitomi21 <kubastach2001@gmail.com>
2023-03-22 18:42:57 +0100

    Add LeRU and change data to train

--- network_model.py
+++ network_model.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def relu(x):
-        return max(0, x)
+    return np.maximum(0, x)
     
 def sigmoid(x):
     return 1/(1+np.exp(-x))
     
-def sigmoid_derivative(x):
+def derivative(x):
     return x*(1-x)
 
 class NeuralNetwork:
@@ -14,16 +14,17 @@ class NeuralNetwork:
         self.input = x
         self.weight1 = np.random.rand(np.shape(self.input)[1], n_neurons)
         self.weight2 = np.random.rand(n_neurons, 1)
+        self.biases = np.zeros((1, n_neurons))
         self.y = y
         self.output = np.zeros(np.shape(y)[0])
     
     def feed_forward(self):
-        self.layer = sigmoid(np.dot(self.input, self.weight1))
+        self.layer = sigmoid(np.dot(self.input, self.weight1) + self.biases)
         self.output = sigmoid(np.dot(self.layer, self.weight2))
 
     def propra_back(self):
-        self.weight2 += np.dot(self.layer.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        self.weight1 += np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weight2.T) * sigmoid_derivative(self.layer)))
+        self.weight2 += np.dot(self.layer.T, (2*(self.y - self.output) * derivative(self.output)))
+        self.weight1 += np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * derivative(self.output), self.weight2.T) * derivative(self.layer)))
 
     def train(self):
          self.feed_forward()
@@ -31,11 +32,17 @@ class NeuralNetwork:
 
 
 
-X = np.array([1,2,3,4,5,5,4,3,2,1])
+X = np.array([0.1,0.2,0.3,0.4,0.5,0.5,0.4,0.3,0.2,0.1])
 X = np.reshape(X, (np.shape(X)[0], 1))
-y = np.array([2,3,4,5,5,4,3,2,1,0])
+y = np.array([0.2,0.3,0.4,0.5,0.5,0.4,0.3,0.2,0.1,0])
 y = np.reshape(y, (np.shape(y)[0], 1))
 network = NeuralNetwork(X, y, 5)
+
+for i in range(1000):
+     print(np.reshape(network.output, (1, np.shape(network.output)[0])))
+     network.train()
+
+
 
 '''
 print(network.input)
@@ -57,7 +64,3 @@ network.propra_back()
 print(network.weight1)
 print(network.weight2)
 '''
-
-for i in range(100):
-     print(np.reshape(network.output, (1, np.shape(network.output)[0])))
-     network.train()
