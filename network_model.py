e46882c458f57ea1633aaaf09031bc6f6c840ee5
mikitomi21 <kubastach2001@gmail.com>
2023-03-29 12:05:42 +0200

    Add MSE and MAE

--- network_model.py
+++ network_model.py
@@ -3,7 +3,7 @@ from data import *
 import matplotlib.pyplot as plt
 
 def relu(x):
-    return x
+    return max(0,x)
     
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -22,8 +22,8 @@ class NeuralNetwork:
         self.rate_learning = 0.00001
     
     def feed_forward(self):
-        self.layer = relu(np.dot(self.input, self.weight1) + self.biases)
-        self.output = relu(np.dot(self.layer, self.weight2))
+        self.layer = np.dot(self.input, self.weight1) + self.biases
+        self.output = np.dot(self.layer, self.weight2)
 
     def propra_back(self):
         output_error = (2*(self.y - self.output) * derivative(self.output))
@@ -38,40 +38,15 @@ class NeuralNetwork:
         self.y = y_train
         self.feed_forward()
         self.propra_back()
-
-
-train_data = get_data()
-
-x_train = train_data["Avg_Temp_Pre_Day"].to_numpy()
-x_train = np.reshape(x_train, (np.shape(x_train)[0], 1))
-y_train = train_data["Avg_Temp"].to_numpy()
-y_train = np.reshape(y_train, (np.shape(y_train)[0], 1))
-x_train = standardize(x_train, x_train)
-y_train = standardize(y_train, y_train)
-
-network = NeuralNetwork(x_train[0:7].T, y_train[7], 2)
-
-sum1 = 0
-sum2 = 0
-lenght = len(x_train)-7
-y_output = []
-y_pred = []
-for i in range(lenght):
-    network.train(x_train[i:i+7].T, y_train[i+7])
-    y_output.append(y_train[i+7])
-    y_pred.append(network.output[0])
-    #print(f"\ni:{i}")
-    #print(f"y:{network.y}")
-    #print(f"output:{network.output}")
-    if i > 600:
-        sum1 += (network.output - network.y)
-        sum2 += pow((network.output - network.y), 2)
-print(f"MSE: {sum2/lenght}")
-print(f"MAE: {sum1/lenght}")
-   
-x = np.arange(lenght)
-plt.plot(x, y_output, 'b')
-plt.plot(x, y_pred, 'r')
-plt.xlabel('Avg_Temp_Pre_Day')
-plt.ylabel('Avg_Temp')
-plt.show()
+    
+    def mse(self, y_output, y_pred):
+        mse = 0
+        for i in range(len(y_output)):
+            mse += pow((y_output[i] - y_pred[i]),2)
+        return mse/len(y_output)
+    
+    def mae(self, y_output, y_pred):
+        mae = 0
+        for i in range(len(y_output)):
+            mae += abs(y_output[i] - y_pred[i])
+        return mae/len(y_output)
