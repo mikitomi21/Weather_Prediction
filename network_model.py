e53daf3e894b60bbc0be6a06f2defc56b747784a
Kuba Stachowicz <kubastach2001@gmail.com>
2023-04-22 13:24:18 +0200

    Revert "Add new feature 'sum_of_fall'"
    This reverts commit 902b7368f2f60f8bf3484bd75ecfa36d08987001.

--- network_model.py
+++ network_model.py
@@ -15,7 +15,7 @@ class NeuralNetwork:
     def __init__(self, x, y, n_neurons):
         self.input = x
         self.weight1 = np.random.rand(np.shape(self.input)[1], n_neurons)
-        self.weight2 = np.random.rand(n_neurons, 2)
+        self.weight2 = np.random.rand(n_neurons, 1)
         self.biases = np.zeros((1, n_neurons))
         self.y = y
         self.output = np.zeros(np.shape(y)[0])
