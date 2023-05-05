09bf5155af5a0ee4ba506db6a546b1ee851c4c15
Marcin Stenka <mastenka@wp.pl>
2023-05-05 11:59:32 +0200

    s_d_t data added

--- downloader.py
+++ downloader.py
@@ -3,11 +3,13 @@ import os
 
 indexes = ["205", "375", "600", "295", "424", "155"]
 DOWNLOAD_FOLDER = "C:\\Users\\maste\\Downloads\\"
+PROJECT_DATA_FOLDER = "C:\\Users\\maste\\source\\repos\\Sztuczna inteligencja\\Weather_Prediction\\data\\"
 
-for year in range(1966, 1997, 5):
+
+for year in range(2001, 2023):
     for index in indexes:
-        url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/" + str(year) + "_" + str(year+4)+ "/" + str(year) + "_" + str(year+4) + "_" + index + "_s.zip"
+        url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/" + str(year)+"_" +str(year+4)+ "/" + str(year)+"_" +str(year+4) + "_"+ index+"_s.zip"
         response = requests.get(url)
         if not os.path.exists(DOWNLOAD_FOLDER+str(year)+"_"+index+"_s.zip"):
-            open(DOWNLOAD_FOLDER + str(year) + "_" + str(year+4)+"_"+index+"_s.zip", "wb").write(response.content)
+            open(DOWNLOAD_FOLDER + str(year) +"_"+str(year+4)+ "_"+index+"_s.zip", "wb").write(response.content)
         
--- unziper_s.py
+++ unziper_s.py
@@ -9,40 +9,37 @@ import shutil
 DOWNLOAD_FOLDER = "C:\\Users\\maste\\Downloads\\"
 PROJECT_DATA_FOLDER = "C:\\Users\\maste\\source\\repos\\Sztuczna inteligencja\\Weather_Prediction\\data\\"
 YEAR_START = 1966
-YEAR_END = 1997
-indexes = ["140", "205", "295", "375", "424", "600"]
+YEAR_END = 2000
+indexes = ["155"]
 
 for year in range (YEAR_START, YEAR_END, 5):
+
     for index in indexes:
+        # path = DOWNLOAD_FOLDER + str(year) + "_" + index + "_s.zip"
         path = DOWNLOAD_FOLDER + str(year) + "_" + str(year+4) + "_" + index+ "_s.zip"
         print(path)
         if not os.path.exists(path):
             break
         with zipfile.ZipFile(path, 'r') as zip_ref:
             zip_ref.extractall(DOWNLOAD_FOLDER)
-        fileToDelete = DOWNLOAD_FOLDER +"s_d_t_"+index+"_"+ str(year) + "_" + str(year+4) + ".csv"
-        if index == "155" and not os.path.exists(fileToDelete):
-            fileToDelete = DOWNLOAD_FOLDER +"s_d_t_140_"+ str(year) + "_" + str(year+4) + ".csv"
-            if not os.path.exists(fileToDelete):
-                fileToDelete = DOWNLOAD_FOLDER +"s_d_t_150_"+ str(year) + "_" + str(year+4) + ".csv"
-        
+
+        print(path)
+        # fileToDelete = DOWNLOAD_FOLDER +"s_d_"+index+"_" + str(year) + ".csv"
+        fileToDelete = DOWNLOAD_FOLDER +"s_d_"+index+"_"+ str(year) + "_" + str(year+4) + ".csv"
+
         os.remove(fileToDelete)
         os.remove(path)
 
     for index in indexes:
         
-        newPath = PROJECT_DATA_FOLDER + str(year) + "_" + str(year+4) 
+        # newPath = PROJECT_DATA_FOLDER + str(year)
+        newPath = PROJECT_DATA_FOLDER + str(year) + "_" + str(year+4)
         if not os.path.exists(newPath):
             os.makedirs(newPath)
-        file = "s_d_" + index + "_" + str(year) + "_" + str(year+4) + ".csv"
-        
+        # file = "s_d_t_" + index + "_" + str(year) + ".csv"
+        file = "s_d_t_" + index + "_" + str(year) + "_" + str(year+4) + ".csv"
+
         oldPath = DOWNLOAD_FOLDER + file
-        if index == "155" and not os.path.exists(oldPath):
-            file = "s_d_140_" + str(year) + "_" + str(year+4) + ".csv"
-            oldPath = DOWNLOAD_FOLDER + file
-            if not os.path.exists(oldPath):
-                file = "s_d_150_" + str(year) + "_" + str(year+4) + ".csv"
-                oldPath = DOWNLOAD_FOLDER + file
         if not os.path.exists(newPath+"\\"+file) and os.path.exists(oldPath):
             shutil.move(oldPath, newPath)
         else:
